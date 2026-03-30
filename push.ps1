91243d88a411b39ce65a0f2f683a8514ba2a34b6
Karan Singh Shekhawat <shekhawatk271@gamil.com>
2026-03-30 19:36:50 +0530

    Update API URL for Vercel deployment

--- push.ps1
+++ push.ps1
@@ -1,7 +1,3 @@
-git init
 git add .
-git commit -m "Initialize project"
-git branch -M main
-git remote remove origin
-git remote add origin https://github.com/k0-123/LiveHero.git
-git push -u origin main
+git commit -m "Update API URL for Vercel deployment"
+git push
